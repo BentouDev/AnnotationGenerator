bb048350c8302b7fddd939fd1f8734b966dddbec
MrJaqbq <bentkowski.jakub@gmail.com>
2017-09-11 21:45:24 +0200

    Reflection data classes

---
+++ include/ReflectionInfo.h
@@ -0,0 +1,109 @@
+//
+// Created by bentoo on 11.09.17.
+//
+////////////////////////////////////////////////////////////////////////////////////
+#ifndef ANNOTATIONGENERATOR_REFLECTIONINFO_H
+#define ANNOTATIONGENERATOR_REFLECTIONINFO_H
+////////////////////////////////////////////////////////////////////////////////////
+
+#include <string>
+#include <vector>
+
+////////////////////////////////////////////////////////////////////////////////////
+
+namespace ValueType
+{
+    enum TYPE
+    {
+        Value,
+        Pointer,
+        Reference
+    };
+}
+
+namespace AccessType
+{
+    enum TYPE
+    {
+        None = 0,
+        Readable = 1,
+        Writable = 2,
+        Mutable = Readable | Writable,
+        Const = Readable
+    };
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+class MetaInfo
+{
+public:
+    std::string Name;
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+
+using TypeId = int;
+
+class TypeInfo : public MetaInfo
+{
+public:
+    TypeId Id;
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+
+class FieldInfo : public MetaInfo
+{
+public:
+    TypeInfo          BaseType;
+    AccessType::TYPE  AccessType;
+    ValueType::TYPE   ValueType;
+
+    template <typename T>
+    T* GetValue();
+
+    template <typename T>
+    void SetValue(T* value);
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+
+class MethodInfo
+{
+public:
+    TypeInfo ReturnType;
+    std::vector<TypeInfo> Parameters;
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+
+class ClassInfo : public TypeInfo
+{
+public:
+    ClassInfo Super;
+    std::vector<ClassInfo> Interfaces;
+
+    std::vector<FieldInfo>  Fields;
+    std::vector<MethodInfo> Methods;
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+
+class EnumValueInfo : public MetaInfo
+{
+public:
+    template <typename T>
+    T* GetValue();
+};
+
+class EnumInfo : public TypeInfo
+{
+public:
+    TypeInfo Super;
+    std::vector<EnumValueInfo> Values;
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+#endif //ANNOTATIONGENERATOR_REFLECTIONINFO_H
+////////////////////////////////////////////////////////////////////////////////////
