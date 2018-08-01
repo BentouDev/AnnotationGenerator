3d5184af1b2afb45db19830d95857aeacdf0e5df
BentouDev <bentkowski.jakub@gmail.com>
2018-08-01 10:54:34 +0200

    Build as conan package

--- build_clang.sh
+++ build_clang.sh
@@ -3,7 +3,5 @@ export CXX=clang++-5.0
 export CC=clang-5.0
 $CXX --version
 cmake --version
-mkdir bin
-cd bin
-cmake --target all ..
-make
+conan --version
+conan create . bentoudev/yage -s compiler=clang -s build_type=Release
