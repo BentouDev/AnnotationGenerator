67dd3e373fe1796c28aaa5a1d7089e89d8c0d690
BentouDev <bentkowski.jakub@gmail.com>
2018-08-23 11:54:10 +0200

    Upload package as 0.2

--- build_clang.sh
+++ build_clang.sh
@@ -7,4 +7,4 @@ conan --version
 conan create . bentoudev/yage -s compiler=clang -s build_type=Release
 conan remote add yage https://api.bintray.com/conan/bentoudev/yage
 conan user bentoudev -r yage -p $1
-conan upload Agnes/0.1@bentoudev/yage --all -r=yage
+conan upload Agnes/0.2@bentoudev/yage --all -r=yage
