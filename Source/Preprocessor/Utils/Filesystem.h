95ec34b97aca61dc710abd0bfe856ede947e094e
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2018-08-23 18:28:55 +0200

    Reimplemented missing fs::relative for experimental namespace on Clang

--- Source/Preprocessor/Utils/Filesystem.h
+++ Source/Preprocessor/Utils/Filesystem.h
@@ -10,7 +10,20 @@
 namespace fs = std::filesystem;
 #elif __cpp_lib_experimental_filesystem || __has_include(<experimental/filesystem>)
 #include <experimental/filesystem>
+
 namespace fs = std::experimental::filesystem;
+
+namespace std::experimental::filesystem
+{
+    namespace detail
+    {
+        ::fs::path& append(::fs::path& that, typename path::iterator begin, typename path::iterator end);
+    }
+
+    // Return path when appended to a_From will resolve to same as a_To
+    ::fs::path relative( ::fs::path a_From, ::fs::path a_To = ::fs::current_path(), std::error_code code = std::error_code{});
+}
+
 #else
 #error "AnnotationGenerator requires filesystem TS which seems to be lacking!"
 #endif
