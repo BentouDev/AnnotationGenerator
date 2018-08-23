1b6b066cae55758e44664cc67a52c33925a16958
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2018-08-23 18:10:50 +0200

    Better filesystem TS support

--- Source/Preprocessor/Utils/Filesystem.h
+++ Source/Preprocessor/Utils/Filesystem.h
@@ -5,9 +5,17 @@
 #ifndef ANNOTATIONGENERATOR_FILESYSTEM_H
 #define ANNOTATIONGENERATOR_FILESYSTEM_H
 
+#if __cpp_lib_filesystem || __has_include(<filesystem>)
 #include <filesystem>
+namespace fs = std::filesystem;
+#elif __cpp_lib_experimental_filesystem || __has_include(<experimental/filesystem>)
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#else
+#error "AnnotationGenerator requires filesystem TS which seems to be lacking!"
+#endif
 
 using cstring = const char*;
-namespace fs = std::filesystem;
+
 
 #endif //ANNOTATIONGENERATOR_FILESYSTEM_H
